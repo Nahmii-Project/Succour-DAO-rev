17e732427780401d5bb782eabf7e76b7e4399cd2
Blessing Samuel <dicethedev@gmail.com>
2022-11-04 07:43:13 +0100

    Update Navbar.style.ts

--- Succour-Frontend/components/navbar/Navbar.style.ts
+++ Succour-Frontend/components/navbar/Navbar.style.ts
@@ -4,7 +4,7 @@ import styled from 'styled-component';
 export const NavbarContainer = styled.nav`
  position: fixed;
  width: 100%;
- height: ${(( props :any) => (props.click ? "100vh" : "105px" )};
+ height: ${(((props :any)) => (props.click ? "100vh" : "105px" )};
  background: #070606;
  display: flex;
  z-index: 1000;
