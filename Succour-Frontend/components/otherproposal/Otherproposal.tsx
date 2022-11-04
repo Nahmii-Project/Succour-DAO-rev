e50e45adab42edc78723be326223852cc9ec8272
Blessing Samuel <dicethedev@gmail.com>
2022-11-04 08:18:11 +0100

    Update Otherproposal.tsx

--- Succour-Frontend/components/otherproposal/Otherproposal.tsx
+++ Succour-Frontend/components/otherproposal/Otherproposal.tsx
@@ -12,45 +12,45 @@ const Otherproposal = () => {
      }
 
   return (
-//     <div className={styles.otherproposal}>
-//       <div className={styles.wrapper}>
-//           <div className={styles.container}>
-//                <div className={styles.header}>
-//                 <h1 className={styles.title}>Other proposals</h1>
-//                </div>
+    <div className={styles.otherproposal}>
+      <div className={styles.wrapper}>
+          <div className={styles.container}>
+               <div className={styles.header}>
+                <h1 className={styles.title}>Other proposals</h1>
+               </div>
 
-//                 <Link href={`/ProposePage/${id}`}>
-//                <div className={styles.otherproposal_content}>
-//                             {
-//                     data?.slice(0, visible)?.map(({ id, time, title, address, type, problem }) => {
-//                        return (
-//                     <div className={styles.otherproposal_item} key={id}>
-//                     <div className={styles.otherproposal_text}>
-//                       <h2 className={styles.item_title}>{title}</h2>
-//                        <p className={styles.item_address}>{address}</p>
-//                       <h1 className={styles.problem_title}>{problem}</h1>
+                <Link href="">
+               <div className={styles.otherproposal_content}>
+                            {
+                    data?.slice(0, visible)?.map(({ id, time, title, address, type, problem }) => {
+                       return (
+                    <div className={styles.otherproposal_item} key={id}>
+                    <div className={styles.otherproposal_text}>
+                      <h2 className={styles.item_title}>{title}</h2>
+                       <p className={styles.item_address}>{address}</p>
+                      <h1 className={styles.problem_title}>{problem}</h1>
                     
-//                     <div className={styles.donation_duration}>
-//                          <span className={styles.duration_type}>{type}</span>
-//                          <span className={styles.time}>{time}</span>
-//                      </div>
-//                     </div>
-//                     </div>
-//                       )
-//                            })
-//                          }
-//                </div>
-//                </Link>
-//                 <div className={styles.viewmore_center}>
-//                 <button onClick={handleMoreProjectItem}
-//                 className={styles.viewmore_btn}
-//                 >
-//                 View more
-//                 </button>
-//               </div>
-//           </div>
-//       </div>
-//     </div>
+                    <div className={styles.donation_duration}>
+                         <span className={styles.duration_type}>{type}</span>
+                         <span className={styles.time}>{time}</span>
+                     </div>
+                    </div>
+                    </div>
+                      )
+                           })
+                         }
+               </div>
+               </Link>
+                <div className={styles.viewmore_center}>
+                <button onClick={handleMoreProjectItem}
+                className={styles.viewmore_btn}
+                >
+                View more
+                </button>
+              </div>
+          </div>
+      </div>
+    </div>
   )
 }
 
